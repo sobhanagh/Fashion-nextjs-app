5fc0300000230f0cb57b78ab3d67c6fb71b999d2
sobhan <sobhanaghajani893@gmail.com>
2024-08-17 19:20:17 +0330

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
+import HomePage from "./component/HomePage";
 
 
 export default function Home() {
   return (
     <main>
-      <div className="h-screen">
-        <div className="bg-greenColor w-full h-screen absolute inset-0 -z-10"></div>
+      <div className="mb-28">
+        <HomePage />
       </div>
-      <div className="bg-red-500 mt-1">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam rem natus fugit ratione illum similique, accusantium nobis perferendis, ad fugiat molestias dignissimos quisquam non vero, assumenda repellendus voluptates cupiditate veniam!</p>
+      <div className="bg-red-600">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nostrum quaerat ex sapiente sint illo autem, impedit tempora odit tempore est quae earum repellat sed in eveniet quas porro? Dolor?z</p>
       </div>
-    </main>
+    </main >
   );
 }
